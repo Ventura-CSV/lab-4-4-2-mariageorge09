0c19f39944989b7e312f0d87fe1016390332dbc1
Maria <emailtomariageorge@gmail.com>
2024-06-26 17:00:00 -0700

    Excercise 4-2

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    while True:
+        try:
+            user_num = int(input('Enter a number: '))
+            number = user_num
+        except ValueError:
+            print ('Input must be numeric')
+            continue
+        else:
+            print ('Output: ')
+            print (number)
+            break
+        
+        
+
 
     ########################################
     # Do not delete the return statement
